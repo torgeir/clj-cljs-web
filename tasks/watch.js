4b601d32e9847b2a94a169a9c833d93ee86e7248
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-09-17 16:51:16 +0200

    Watch images.

--- tasks/watch.js
+++ tasks/watch.js
@@ -4,8 +4,9 @@ var config = require('./config');
 
 module.exports = function () {
   return function () {
-    gulp.watch(config.FILES_LESS,  ['less']);
+    gulp.watch(config.FILES_LESS,   ['less']);
+    gulp.watch(config.FILES_IMAGES, ['images']);
   };
 };
 
-module.exports.deps = ['less', 'fonts'];
+module.exports.deps = ['less', 'fonts', 'images'];
